ddc3b1cf0e2ba4c9c7e3370ca68c152f898da350
Oleksadr Haiu <sashagayu1@gmail.com>
2025-11-19 14:30:18 +0200

    feat: підключити shop у головних маршрутах

--- base/urls.py
+++ base/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-# Поки що в проєкті лише стандартна сторінка адміністратора.
+# Тепер до головного маршрутизатора підключаю і свій застосунок.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shop.urls')),
 ]
